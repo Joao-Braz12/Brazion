de6e55898b24ba61110b2daaff0129cd2aaf2483
SrToffee <jp.brazpoke@gmail.com>
2025-05-02 11:33:50 +0100

    50%

--- app/(main)/_components/navegation.tsx
+++ app/(main)/_components/navegation.tsx
@@ -1,21 +1,21 @@
 "use client"
 
 import { cn } from "@/lib/utils";
-import { ChevronLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronLeft, MenuIcon, PlusCircle, Search, Settings, SquareActivity } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { use, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navegation = () => {
 	const pathname = usePathname();
 	const isMobile = useMediaQuery("(max-width: 768px)");
 
-	const documents = useQuery(api.documents.get);
 	const create = useMutation(api.documents.create);
 
 	const isResizingRef = useRef(false);
@@ -137,10 +137,8 @@ export const Navegation = () => {
 						label="New Page"
 						icon={PlusCircle}/>
 				</div>
-				<div className="nt-4">
-					{documents?.map((document) => (
-						<p key={document._id}>{document.title}</p>
-					))}
+				<div className="mt-4">
+					<DocumentList />
 				</div>
 				<div
 					onMouseDown={handleMouseDown}
--- convex/documents.ts
+++ convex/documents.ts
@@ -1,31 +1,86 @@
-import {v} from "convex/values";
+import { v } from "convex/values";
 
 import { mutation, query } from "./_generated/server";
-import{Doc, Id} from "./_generated/dataModel"
+import { Doc, Id } from "./_generated/dataModel"
+import { error } from "console";
 
-export const get = query({
-	handler: async (ctx) => {
-		const identity = await ctx.auth.getUserIdentity();
+export const archive = mutation({
+	args: {
+	id: v.id("documents")
+	},
+	handler: async (ctx, args) => {
+		const id = await ctx.auth.getUserIdentity();
 
-		if(!identity)
+		if (!id)
 			throw new Error("Not authenticated");
 
-		const documents = await ctx.db.query("documents").collect();
+		const userId = id.subject;
 
-		return documents;
+		const existingdocument = await ctx.db.get(args.id);
+
+		if (!existingdocument)
+			throw new Error("Document not found");
+
+		if (existingdocument.userId !== userId)
+			throw new Error("Not authorized to archive this document");
+
+		const recursiveArchive = async (id: Id<"documents">) => {
+			const children = await ctx.db.query("documents")
+				.withIndex("byUserId_parent", (q) => (
+					q.eq("userId", userId)
+						.eq("parentDocument", id)
+				)).collect();
+			for (const child of children) {
+				await ctx.db.patch(child._id, {
+					isArchived: true
+				});
+				await recursiveArchive(child._id);
+			}
+		}
+
+		const document = await ctx.db.patch(args.id, {
+			isArchived: true
+		});
+
+		await recursiveArchive(args.id);
+
+		return document;
+	}
+})
+
+export const getSidebar = query({
+	args: {
+		parentDocument: v.optional(v.id("documents"))
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+
+		if (!identity)
+			throw new Error("Not authenticated");
+
+		const userId = identity.subject;
+
+		const documents = await ctx.db.query("documents")
+			.withIndex("byUserId_parent", (q) =>
+				q.eq("userId", userId)
+					.eq("parentDocument", args.parentDocument))
+			.filter((q) => q.eq(q.field("isArchived"), false))
+			.order("desc")
+			.collect();
+		return documents
 	}
 })
 
 
 export const create = mutation({
-	args:{
+	args: {
 		title: v.string(),
 		parentDocument: v.optional(v.id("documents"))
 	},
-	handler: async(ctx, args) =>{
+	handler: async (ctx, args) => {
 		const identity = await ctx.auth.getUserIdentity();
 
-		if(!identity)
+		if (!identity)
 			throw new Error("Not authenticated");
 
 		const userId = identity.subject;
