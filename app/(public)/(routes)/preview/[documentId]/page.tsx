3d73b880e609c80d4f44617d9d1163564293bd3e
SrToffee <jp.brazpoke@gmail.com>
2025-06-03 11:22:38 +0100

    deploy_5

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -6,25 +6,20 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
+import { usePathname } from "next/navigation";
 
-interface DocumentIdPageProps {
-	params: {
-		documentId: Id<"documents">;
-	}
-}
-
-const DocumentIdPage = ({
-	params
-}:DocumentIdPageProps) => {
-	const Editor = useMemo(() => dynamic(() => import("@/components/editor"), {ssr: false}), []);
-
-	const document = useQuery(api.documents.getById, {documentId  : params.documentId});
+const DocumentIdPage = () => {
+	const Editor = useMemo(() => dynamic(() => import("@/components/editor"), { ssr: false }), []);
+	const pathname = usePathname();
+	const parts = pathname.split("/");
+	const documentId = parts[parts.length - 1] as Id<"documents">;
+	const document = useQuery(api.documents.getById, { documentId: documentId });
 
 	const update = useMutation(api.documents.update);
 
-	const onChange = (content:string) => {
+	const onChange = (content: string) => {
 		update({
-			id: params.documentId,
+			id: documentId,
 			content
 		});
 	}
@@ -34,16 +29,16 @@ const DocumentIdPage = ({
 			<div>
 				<div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
 					<div className="space-y-4 pl-8 pt-4">
-						<Skeleton className="h-14 w-[80%]"/>
-						<Skeleton className="h-4 w-[60%]"/>
-						<Skeleton className="h-4 w-[40%]"/>
-						<Skeleton className="h-4 w-[20%]"/>
+						<Skeleton className="h-14 w-[80%]" />
+						<Skeleton className="h-4 w-[60%]" />
+						<Skeleton className="h-4 w-[40%]" />
+						<Skeleton className="h-4 w-[20%]" />
 					</div>
 				</div>
 			</div>
 		);
 
-	if (document === null){
+	if (document === null) {
 		return (
 			<div>
 				Document not found
@@ -53,11 +48,11 @@ const DocumentIdPage = ({
 
 	return (
 		<div className="pb-40">
-			<div className="h-35vh"/>
+			<div className="h-35vh" />
 			<div
-			className="md:max-w-3xl lg:max-4xl mx-auto">
+				className="md:max-w-3xl lg:max-4xl mx-auto">
 				<Toolbar preview initialData={document} />
-				<Editor editable ={false}
+				<Editor editable={false}
 					onChange={onChange}
 					initialContent={document.content}
 				/>
